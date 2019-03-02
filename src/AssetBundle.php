f2939783c9a8bdb792752362df85b86e3b3138b4
Jomon Johnson <jomon.entero@gmail.com>
2019-03-02 14:40:38 +0530

    init

--- src/AssetBundle.php
+++ src/AssetBundle.php
@@ -6,15 +6,15 @@ class AssetBundle extends \yii\web\AssetBundle
 {
     public $sourcePath = '@codexten/yii/web/assets';
     public $js = [
-        'js/enyii.js',
-        'js/jquery.are-you-sure.js',
+//        'js/enyii.js',
+//        'js/jquery.are-you-sure.js',
     ];
     public $css = [
-        'css/enyii.css',
-        'css/helpers.css',
+//        'css/enyii.css',
+//        'css/helpers.css',
     ];
     public $depends = [
         'yii\web\YiiAsset',
-        'yii\web\JqueryAsset',
+//        'yii\web\JqueryAsset',
     ];
 }
