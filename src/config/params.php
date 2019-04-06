8c0a26f7adcc3cf1c96ea26d33854c8a27d0455b
Jomon Johnson <jomon.entero@gmail.com>
2019-04-06 10:57:16 +0530

    init

--- src/config/params.php
+++ src/config/params.php
@@ -3,7 +3,7 @@ return [
     'app.id' => 'webApp',
     'app.name' => 'webApp',
 
-    'cookieValidationKey' => $_ENV['COOKIE_VALIDATION_KEY'],
+    'cookieValidationKey' => @$_ENV['COOKIE_VALIDATION_KEY'],
 
     'favicon' => '/favicon.ico',
 ];
