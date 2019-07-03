ea576b65ae47b193bd0bba1812d379c2a0d841e8
Jomon Johnson <jomon.entero@gmail.com>
2019-07-03 12:10:27 +0530

    member user index

--- src/views/crud/index.php
+++ src/views/crud/index.php
@@ -1,13 +1,14 @@
 <?php
 
 use codexten\yii\web\widgets\IndexPage;
+use yii\helpers\Inflector;
 
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 /* @var $searchModel */
 /* @var $_params_ array */
 
-$this->title = Yii::t('gnt:core', 'Staff Users');
+$this->title = Inflector::pluralize(Inflector::camel2words(Inflector::id2camel(Yii::$app->controller->id)))
 ?>
 
 <?php $page = IndexPage::begin([
@@ -22,7 +23,7 @@ $this->title = Yii::t('gnt:core', 'Staff Users');
 
 <?php $page->beginContent('table') ?>
 
-<?= $this->render('_grid', $_params_) ?>
+<?= $this->render('index/_grid', $_params_) ?>
 
 <?php $page->endContent() ?>
 
