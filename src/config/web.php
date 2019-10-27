0da35702f90936e6472ba028ed7706704bf8a7bb
Jomon Johnson <jomon.entero@gmail.com>
2019-10-27 19:32:38 +0530

    minify

--- src/config/web.php
+++ src/config/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use codexten\yii\web\components\View;
 use codexten\yii\web\controllers\SiteController;
 use codexten\yii\web\ThemeManager;
 use codexten\yii\web\UrlManager;
@@ -52,6 +53,19 @@ return array_filter([
         ],
         'view' => [
             'class' => '\codexten\yii\web\components\View',
+            'enableMinify' => true,
+            'concatCss' => true, // concatenate css
+            'minifyCss' => true, // minificate css
+            'concatJs' => true, // concatenate js
+            'minifyJs' => true, // minificate js
+            'minifyOutput' => true, // minificate result html page
+            'webPath' => '@web', // path alias to web base
+            'basePath' => '@webroot', // path alias to web base
+            'minifyPath' => '@webroot/minify', // path alias to save minify result
+            'jsPosition' => [\yii\web\View::POS_END], // positions of js files to be minified
+            'forceCharset' => 'UTF-8', // charset forcibly assign, otherwise will use all of the files found charset
+            'expandImports' => true, // whether to change @import on content
+            'compressOptions' => ['extra' => true], // options for compress
             'theme' => [
                 'pathMap' => [
                     '@app/views' => [
